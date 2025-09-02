79088485b0bb5547ec6885aac3e9ba37fe486737
Zhassulan Tokbayev <31761872+Zhassulan@users.noreply.github.com>
2025-09-03 00:47:01 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   staging: false,
-  WS_SERVER: 'wss://b2b.tele2.kz/ncp-messager',
-  API_URL: 'https://b2b.tele2.kz/ncp/api/v1',
-  LDAP_API_URL: 'https://b2b.tele2.kz/api/v1/ldap'
+  WS_SERVER: 'wss://b2b.ttk.kz/ncp-messager',
+  API_URL: 'https://b2b.ttk.kz/ncp/api/v1',
+  LDAP_API_URL: 'https://b2b.ttk.kz/api/v1/ldap'
 };
