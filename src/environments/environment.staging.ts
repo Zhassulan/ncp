3c3067df6cc5b02c32cc8774771a54d292bfb7f5
Zhassulan Tokbayev <31761872+Zhassulan@users.noreply.github.com>
2025-09-03 00:47:10 +0500

    Update environment.staging.ts

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   staging: true,
-  WS_SERVER: 'wss://b2b.tele2.kz/ncp-messager',
-  API_URL: 'https://b2b.tele2.kz/ncp/api-test/api/v1',
-  LDAP_API_URL: 'https://b2b.tele2.kz/api/v1/ldap-test'
+  WS_SERVER: 'wss://b2b.ttk.kz/ncp-messager',
+  API_URL: 'https://b2b.ttk.kz/ncp/api-test/api/v1',
+  LDAP_API_URL: 'https://b2b.ttk.kz/api/v1/ldap-test'
 };
